bb7e6ad67957e310472b4b199da4034357c3a4f1
Cristian Garabagiu <cristian.garabagiu@gmail.com>
2018-06-26 23:15:51 +0300

    Created Header component

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <span className="title">Timeline</span>
+      <input
+        type="text"
+        className="searchInput"
+        placeholder="Search ..."/>
+      <FontAwesomeIcon icon={Icons.faSearch} className="searchIcon"/>
+    </div>
+  );
 };
 
 export default Header;
