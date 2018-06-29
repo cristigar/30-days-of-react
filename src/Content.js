2b81e31eef72a3eb492bcdd5a340bf49f06a873d
Cristian Garabagiu <cristian.garabagiu@gmail.com>
2018-06-29 08:18:12 +0300

    Finished Content component

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <div className="content">
+      <div className="line" />
+      <div className="item">
+        <div className="avatar">
+          <img
+            alt='Doug'
+            src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+            title="Doug"
+          />
+        </div>
+        <span className="time">An hour ago</span>
+        <p>Ate lunch</p>
+        <div className="commentCount">
+          <span className="commentText">2</span>
+          <FontAwesomeIcon icon={Icons.faComment} className="commentIcon" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
